a567810424871e61f66c9f5722295f3121e210a3
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2022-08-22 15:22:12 +0200

    Se instala la dependencia de desarrollo gulp-webp. Se actualiza gulpfile para hacer uso de gulp-webp. Se crean las imagenes en formato webp con salida en la carpeta assets/img

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const { src, dest } = require('gulp');
+const { src, dest, series } = require('gulp');
 
 // Imagenes
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function imagenes(done) {
 	const options = {
@@ -15,4 +16,16 @@ function imagenes(done) {
 	done();
 }
 
-exports.imagenes = imagenes;
+function versionWebp(done) {
+	const options = {
+		quality: 50
+	};
+	src('assets/img/**/*.{png,jpg}')
+		.pipe(webp(options))
+		.pipe(dest('assets/img'))
+
+	done();
+}
+
+exports.imagenes = series(imagenes, versionWebp);
+exports.versionWebp = versionWebp;
